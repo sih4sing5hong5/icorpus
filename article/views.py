66d0c449ab48d119efe577f561accdea7a307789
Ihc <ihcaoe@gmail.com>
2014-01-31 14:12:05 +0800

    照範例改一个　丞宏

--- article/views.py
+++ article/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from article.models import Article
+
+def index(request):
+    latest_poll_list = Article.objects.order_by('date')[:5]
+    output = ', '.join([p.title for p in latest_poll_list])
+    return HttpResponse(output)
+
+
 # Create your views here.
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
--- iCorpus/urls.py
+++ iCorpus/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^article/', include('article.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
