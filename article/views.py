7b5047d1db5a87a7788d57ba99d4e4786dac23c9
Ihc <ihcaoe@gmail.com>
2014-01-31 21:44:45 +0800

    加樣板　丞宏

--- article/views.py
+++ article/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from article.models import Article
 
 def index(request):
     latest_poll_list = Article.objects.order_by('date')[:5]
-    output = ', '.join([p.title for p in latest_poll_list])
-    return HttpResponse(output)
+#    output = ', '.join([p.title for p in latest_poll_list])
+#    return HttpResponse(output)
+    template = loader.get_template('article/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
 
 
 # Create your views here.
