dc10432ba5887966b67e3cb88c68b13dba51f38e
Ihc <ihcaoe@gmail.com>
2014-02-21 10:28:12 +0800

    加行號 丞宏

--- article/文章表格.py
+++ article/文章表格.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.forms import Textarea
 from article.models import Article
 
 class 文章全部表格(ModelForm):
@@ -29,9 +30,17 @@ class 改國語斷詞表格(ModelForm):
 	class Meta:
 		model = Article
 		fields = ['origin_title', 'title', 'origin_content', 'content']
+		widgets = {
+			'origin_content': Textarea(attrs={'class':'文章','wrap': 'off'}),
+			'content': Textarea(attrs={'class':'文章','wrap': 'off'}),
+		}
 
 
 class 改閩南語翻譯表格(ModelForm):
 	class Meta:
 		model = Article
 		fields = ['title', 'title_translation', 'content', 'JiaoLuo']
+		widgets = {
+			'content': Textarea(attrs={'class':'文章','wrap': 'off'}),
+			'JiaoLuo': Textarea(attrs={'class':'文章','wrap': 'off'}),
+		}
