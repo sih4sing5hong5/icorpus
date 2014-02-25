e5a836f55a2a888d756b3df818538464cf2601a1
Ihc <ihcaoe@gmail.com>
2014-02-25 18:51:40 +0800

    予全部違法的網址攏走去首頁 丞宏

--- 文章/urls.py
+++ 文章/urls.py
@@ -4,7 +4,6 @@ from 文章 import views
 
 urlpatterns = patterns('',
     # ex: /polls/
-    url(r'^$', views.index, name='首頁'),
     url(r'^文章/$', views.index),
     url(r'^登入', views.登入, name='登入'),
     url(r'^登出$', views.登出, name='登出'),
@@ -17,6 +16,8 @@ urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/改閩南語翻譯/$', views.改閩南語翻譯, name='改閩南語翻譯'),
     
     url(r'^揣文章$', views.揣文章, name='揣文章'),
+    
+    url(r'^.*$', views.index, name='首頁'),
 #     # ex: /polls/5/vote/
 #     url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
 #     url(r'^(?P<pk>\d+)/editclass/$', views.EditView.as_view(), name='editClass'),
