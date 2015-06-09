e00545275fa25b37f9bb4b5da76adde3b271b222
薛丞宏 <ihcaoe@gmail.com>
2015-06-09 14:04:54 +0800

    資料改用yield

--- 文章/對語料庫網站掠資料落來.py
+++ 文章/對語料庫網站掠資料落來.py
@@ -14,7 +14,6 @@ class 對語料庫網站掠資料落來:
 		網頁檔案.close()
 		# icorpus.iis.sinica.edu.tw/{0}/揣翻譯對應/
 		掠文章 = 'http://icorpus.iis.sinica.edu.tw/{0}/%E6%8F%A3%E7%BF%BB%E8%AD%AF%E5%B0%8D%E6%87%89'
-		資料 = []
 		for 文章號碼 in json.loads(網頁.decode('UTF-8'))[:]:
 			文章內容檔案 = urllib.request.urlopen(掠文章.format(文章號碼))
 			文章內容=文章內容檔案.read()
@@ -23,10 +22,11 @@ class 對語料庫網站掠資料落來:
 #			 print(掠文章.format(文章號碼))
 #			 print(文章號碼)
 			翻譯 = json.loads(文章內容.decode('UTF-8'))
-			資料.append(翻譯)
-		return 資料
+			yield 翻譯
 		
 if __name__ == '__main__':
 	掠資料落來 = 對語料庫網站掠資料落來()
-	資料 = 掠資料落來.掠資料()
-	print(資料[:10])
+	for 第幾,資料 in enumerate(掠資料落來.掠資料()):
+		if 第幾==10:
+			break
+		print(資料)
